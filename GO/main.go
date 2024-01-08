e02d262b518b3fb996b7451cf0b9cd134df2b774
Valentin Renaud <valentinrenaud@MacBook-Air-de-Valentin-2.local>
2024-01-08 14:49:38 +0100

    GO -> Ouverture d'un fichier .txt

--- GO/main.go
+++ GO/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	var file_name = "Matrice.txt"
+	file, err := os.Open(file_name) // For read access.
+	fmt.Println("\nOuverture du fichier", file_name, "...")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Réussie !!!")
+	file.Close()
 }
