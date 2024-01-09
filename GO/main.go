a994c4f78268a36fb61ac4bd049796299c5abdc5
Valentin Renaud <valentinrenaud@MacBook-Air-de-Valentin-2.local>
2024-01-09 01:47:08 +0100

    GO -> Conversion []byte vers []int

--- GO/main.go
+++ GO/main.go
@@ -4,15 +4,57 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
+func Byte_To_String(CONTENT []byte) []string {
+
+	dataByLine := strings.Split(string(CONTENT), "\n")
+	var line []string
+	var res []string
+
+	for i := 0; i < len(dataByLine); i++ {
+		line = strings.Split(dataByLine[i], " ")
+		for j := 0; j < len(line); j++ {
+			res = append(res, line[j])
+		}
+	}
+	return res
+}
+
+func String_To_Int(DATA []string) []int {
+	var tab []int
+	for i := 0; i < len(DATA); i++ {
+		n, err := strconv.Atoi(DATA[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+		tab = append(tab, n)
+	}
+	return tab
+}
+
 func main() {
-	var file_name = "Matrice.txt"
-	file, err := os.Open(file_name) // For read access.
+
+	file_name := "Matrice.txt"
+
+	file, err := os.Open(file_name) // Permet l'accès au fichier
 	fmt.Println("\nOuverture du fichier", file_name, "...")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Réussie !!!")
+
+	content, err := os.ReadFile(file_name) // Lecture du fichier...
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := Byte_To_String(content)
+	mat := String_To_Int(data)
+
+	fmt.Println(mat[0] + mat[4])
+
 	file.Close()
 }
