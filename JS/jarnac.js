24949a91daf02134f6bb79e9a719add19a919679
Valentin Renaud <valentinrenaud8@gmail.com>
2024-02-04 14:40:10 +0100

    fonction coupJarnac

--- JS/jarnac.js
+++ JS/jarnac.js
@@ -1,6 +1,5 @@
 const prompt = require("prompt-sync")({sigint:true}); //user input library
-
-var checkword = require("check-if-word"), words = checkword('en'); //word check library
+const wordExists = require('word-exists');
 
 var letter_dict = {
     A:14,
@@ -52,25 +51,26 @@ function pick6Letters() {
     return letter_list
 }
 
-function userInput(text,player_array_letters,player) {
+function userInput(text, player_array_letters, player) {
     //works
     var input_check = false;
     var input_exit = false;
-    console.log("Here is your grid: ",player_grid[player]);
+    console.log("Here is your grid: ", player_grid[player]);
     text = "\nPlease enter a word. \n";
     
-    console.log(text,"\nYou have ",player_letters[player].length," letters: ", player_letters[player]);
+    console.log(text,"\nYou have ", player_letters[player].length, " letters: ", player_letters[player]);
     console.log("\nTo go back, type exit() and press enter.\n")
     var user_input = prompt();
     if (user_input=="exit()"){
         console.log("\nGoing back to menu...\n");
         input_exit = true;
-    }else{
-        input_check = inputCheck(user_input,player_array_letters,player);
+    }
+    else {
+        input_check = inputCheck(user_input, player_array_letters, player);
     }
     if (input_exit==false && input_check==false){
         console.clear()
-        return userInput(text,player_array_letters,player);
+        return userInput(text, player_array_letters, player);
     }
     let res = [user_input,input_check];
     //console.log(res);
@@ -78,7 +78,7 @@ function userInput(text,player_array_letters,player) {
 
 }
 
-function inputCheck(user_input,player_array_letters,player) {
+function inputCheck(user_input, player_array_letters, player) {
     //returns true if word passes check, false otherwise
     //3 letters min, inside the noms_communs dic (feminins, pluriels, verbes à tous les temps/modes/personnes),
     // exclus (prefixes, interjections, sigles, symboles, mots composés)
@@ -91,20 +91,20 @@ function inputCheck(user_input,player_array_letters,player) {
     
     if (split_word.length >= 3){
 
-           for (let letter of split_word){
+        for (let letter of split_word) {
 
-            if (tmp_player.includes(letter)==false){
+            if (tmp_player.includes(letter)==false) {
                 console.log("Invalid Entry. Please only use letters you have.\n");
                 return false;
             }            
             const index = tmp_player.indexOf(letter);
             tmp_player.splice(index,1);
-           }
+        }
 
-           if (words.check(user_input.toLowerCase())){
+        if (wordExists(user_input.toLowerCase()) == true) {
             return true;
-
-        }else{res_string+="Word does not exist...\n";}
+        }
+        else {res_string+="Word does not exist...\n";}
     }
     else{res_string += "Word not long enough, make sure the word is at least 3 letters long.\n";}
     console.log(res_string);
@@ -132,22 +132,22 @@ function pickLetter() {
 function whatToDo(player) {
     while (true) {
         console.clear();
-        console.log("It's player ",player+1,"'s turn")
+        console.log("IT'S PLAYER ", player+1, "'S TURN")
         console.log("\nHere is your grid: ", player_grid[player])
         console.log("\nYou have ",player_letters[player].length," letters: ", player_letters[player]);
-        console.log("\nWhat do you want to do ? (select a number)\n 1 - write a new word \n 2 - turn a word into a new one \n 3 - pass")
+        console.log("\nWhat do you want to do ? (select a number)\n 1 - Write a new word \n 2 - Turn a word into a new one \n 3 - Pass")
         var choice = prompt()
-        if (["1","2","3"].includes(choice)) {
+        if (["1","2","3","4"].includes(choice)) {
             return choice
         }     
-        else{console.log("\Invalid input. Try again.\n")}
+        else {console.log("\Invalid input. Try again.\n")}
     }
 }
 
 function newWord(player){
     console.clear();
     //console.log("Your grid: ", player_grid[player])
-    let user_input = userInput("Make a word: ",player_letters[player],player);
+    let user_input = userInput("Make a word: ", player_letters[player], player);
     console.log(user_input);
     if (user_input[1]){ //if input is valid ==> add the word to grid and do rest of the play.
         player_grid[player].push(user_input[0]);
@@ -162,7 +162,8 @@ function newWord(player){
         }
         //console.log(player_grid[player]);
         return 1;
-    }else if (user_input[0]=="exit()"){
+    }
+    else if (user_input[0]=="exit()"){
         //console.log("\nReturning 0 because exit() was called\n");
         return -1;
     }
@@ -176,7 +177,7 @@ function chooseWord(player){//returns the index of the chosen word inside player
     }
     while (index_array.includes(user_input)==false){
         console.clear();
-        console.log("What word do you want to change ?\n");
+        console.log("What word do you want to use ?\n");
         for (let i = 0; i< index_array.length; i++){
             console.log((i+1), ".  ", player_grid[player][i],"\n");
         }
@@ -190,11 +191,11 @@ function changeWord(player){
     let old_word_index = chooseWord(player);
     var old_word = (player_grid[player][old_word_index].toUpperCase()).split('');
     console.log(old_word);
-    let new_word = prompt();
     console.log("\nPlease enter the new word to be added:\n");
+    let new_word = prompt();
     let new_word_sliced = new_word.split('');
     let player_array = player_letters[player].concat(old_word); 
-    let word_check = inputCheck(new_word,player_array,player);
+    let word_check = inputCheck(new_word, player_array, player);
     console.log(word_check);
     if (word_check==false){
         return word_check;
@@ -220,33 +221,82 @@ function changeWord(player){
 }
 
 function passTurn() {
-    if (i == 0){i=1;}else{i=0;}
+    if (i == 0) {i=1;} else {i=0;}
 }
 
 function exchange3Letters() {
 
 }
 
-function coup2Jarnac(){
-
+function writeJarnac(array, player_to_jarnac, player) {
+    console.clear()
+    player_letters[player_to_jarnac] = array
+    console.log("Write a word with these letters : ", player_letters[player_to_jarnac])
+    var user_input = prompt().toLowerCase()
+    var input_check = inputCheck(user_input, player_letters[player_to_jarnac], player_to_jarnac)
+    if (input_check == true) {
+        player_grid[player].push(user_input);
+        split_word = user_input.split('');
+        for (let letter of split_word) {
+                        console.log("Letters of split_word: ", letter);
+                        const index = player_letters[player_to_jarnac].indexOf(letter.toUpperCase());
+                        if (index > -1) {
+                            player_letters[player_to_jarnac].splice(index,1);
+                            console.log("removed letter from array: ", player_letters[player_to_jarnac])
+                        }
+                    }
+                }
 }
 
-function coup2Jarnac2() {
-    console.log("Hello World!");
+function coupJarnac(player) {
+    console.clear();
+    console.log("IT'S PLAYER ", player+1, "'S TURN\n")
+    player_to_jarnac = (player+1)%2
+    console.log("Here is the player ", player_to_jarnac + 1, "'s grid : ", player_grid[player_to_jarnac])
+    console.log("And here is the player ", player_to_jarnac + 1, "'s letters : ", player_letters[player_to_jarnac])
+    console.log("\nDo you want to JARNAC ? (yes/no)")
+    choice = prompt().toLowerCase()
+    if (choice == "yes") {
+        if (player_grid[player_to_jarnac].length != 0) {
+            console.log("Do you need a word from the grid ? (yes/no)")
+            grid = prompt().toLowerCase()
+            if (grid == "yes") {
+                num = chooseWord(player_to_jarnac)
+                word = (player_grid[player_to_jarnac][num].toUpperCase()).split('')
+                array = player_letters[player_to_jarnac].concat(word)
+                console.log(array)
+                prompt()
+                writeJarnac(array, player_to_jarnac, player)
+                player_grid[player_to_jarnac].splice(num, 1)
+            }
+            else if (grid =="no") {
+                writeJarnac(player_letters[player_to_jarnac], player_to_jarnac, player)
+            }
+        }
+        else {
+            writeJarnac(player_letters[player_to_jarnac], player_to_jarnac, player)
+        }
+    }   
+    else if (choice == "no") 
+        return 
+    else
+        return coupJarnac(player)
 }
 
-function menu(player){
+
+function menu(player) {
     console.clear();
     player_choice = whatToDo(player);
 
-    if (player_choice == 1){ //if player chooses to place a new word
+    if (player_choice == 1) { //if player chooses to place a new word
         let new_word_try = newWord(player);
+        console.log(new_word_try)
         if (new_word_try==1){return turn(player);}
         else if (new_word_try==-1){
             return menu(player);
         }
     }
-   else if (player_choice == 2){
+   else if (player_choice == 2) {
         console.log("User chose to change a word.");
         let change_word_try = changeWord(player);
         if (change_word_try==false){
@@ -258,14 +308,14 @@ function menu(player){
         return turn(player);
         
    }
-   else if (player_choice == 3){
+   else if (player_choice == 3) {
     console.log("User chose to skip turn");
     return 1;
    }
 }
 
 function firstTurn(player) {
-    console.log("It's player ",player+1,"'s turn!\n");
+    console.log("It's player ", player + 1, "'s turn!\n");
     player_letters[player] = pick6Letters()
     //console.log("You have ", player_letters[player].length," letters: ",player_letters[player]);
     return menu(player);
@@ -281,15 +331,17 @@ function turn(player) {
         }
 }
 
-while (game_playing){
-    if (first_turn_played == false){
+while (game_playing) {
+    if (first_turn_played == false) {
         firstTurn(i);
         passTurn();
+        coupJarnac(i);
         firstTurn(i);
         passTurn();
         first_turn_played = true;
     }
-    else{      
+    else {    
+        coupJarnac(i);  
         turn(i);
         passTurn();
     }
